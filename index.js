6027547c561221109c03da4cdf7f84b9a7bf21cc
ZiuChen <457353192@qq.com>
2022-08-03 14:31:02 +0800

    fix: 修正入口函数

--- index.js
+++ index.js
@@ -2,16 +2,8 @@ const checkin = require("./scripts/checkin");
 const seaGold = require("./scripts/seaGold");
 const growth = require("./scripts/growth");
 
-// exports.main_handler = async () => {
-//   await checkin();
-//   await seaGold();
-//   await growth();
-// };
-
-const main_handler = async () => {
-  // await checkin();
-  // await seaGold();
+exports.main_handler = async () => {
+  await checkin();
+  await seaGold();
   await growth();
 };
-
-main_handler();
