2cd90fee0e20e7693dab08e455d768a3a57f54a5
ZiuChen <457353192@qq.com>
2022-08-10 19:37:31 +0800

    fix: 修正入口函数

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const checkin = require("./workflows/checkin");
 const seaGold = require("./workflows/seagold");
 const growth = require("./workflows/growth");
 
-module.exports = main_handler = async () => {
+exports.main_handler = async () => {
   await checkin();
   await seaGold();
   await growth();
