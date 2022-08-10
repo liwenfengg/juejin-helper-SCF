c21b336412cb7ae77a8577b7701c9fca005c2c13
ZiuChen <457353192@qq.com>
2022-08-10 20:53:22 +0800

    feat: 添加更多表情支持

--- workflows/growth/utils/index.js
+++ workflows/growth/utils/index.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const env = require("../utils/env");
+const emojis = require("./emoji");
 const defaultWords = "呃呃";
 const hitokotoURL = "https://v1.hitokoto.cn/" + env.WORDS_API;
 
@@ -20,51 +21,8 @@ const getRandomInt = (min, max) => {
 
 const getRandomEmoji = async () => {
   return new Promise(async r => {
-    const emojis = [
-      "[闭嘴]",
-      "[睡]",
-      "[吃瓜群众]",
-      "[尴尬]",
-      "[发怒]",
-      "[调皮]",
-      "[撇嘴]",
-      "[思考]",
-      "[不失礼貌的微笑]",
-      "[奸笑]",
-      "[抓狂]",
-      "[吐]",
-      "[偷笑]",
-      "[愉快]",
-      "[白眼]",
-      "[傲慢]",
-      "[困]",
-      "[灵光一现]",
-      "[流汗]",
-      "[憨笑]",
-      "[捂脸]",
-      "[奋斗]",
-      "[咒骂]",
-      "[疑问]",
-      "[嘘]",
-      "[晕]",
-      "[衰]",
-      "[骷髅]",
-      "[敲打]",
-      "[再见]",
-      "[擦汗]",
-      "[抠鼻]",
-      "[泣不成声]",
-      "[坏笑]",
-      "[左哼哼]",
-      "[右哼哼]",
-      "[打哈欠]",
-      "[鄙视]",
-      "[委屈]",
-      "[快哭了]",
-      "[摸头]"
-    ];
     const index = getRandomInt(0, emojis.length - 1);
-    return r(emojis[index]);
+    return r(emojis[index].source);
   });
 };
 
