b13c982f7c7d27460c177bbb5fab87df2b89de39
ZiuChen <457353192@qq.com>
2022-11-03 09:57:23 +0800

    feat: 提高支持添加的用户上限

--- workflows/utils/env.js
+++ workflows/utils/env.js
@@ -3,11 +3,15 @@ const env = process.env || {};
 module.exports = {
   /* 掘金Cookie */
   COOKIE: env.COOKIE,
-  /* 多用户掘金Cookie, 当有1名以上用户时填写, 支持同时最多可配置5名用户 */
+  /* 多用户掘金Cookie, 当有1名以上用户时填写, 支持同时最多可配置9名用户 */
   COOKIE_2: env.COOKIE_2,
   COOKIE_3: env.COOKIE_3,
   COOKIE_4: env.COOKIE_4,
   COOKIE_5: env.COOKIE_5,
+  COOKIE_6: env.COOKIE_6,
+  COOKIE_7: env.COOKIE_7,
+  COOKIE_8: env.COOKIE_8,
+  COOKIE_9: env.COOKIE_9,
   /**
    * 邮箱配置
    * user 发件人邮箱, pass, 发件人密码, to收件人
